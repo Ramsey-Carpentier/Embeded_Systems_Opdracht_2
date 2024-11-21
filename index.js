d37669cd80043dd3f7fcb9874e933036b4f61f6e
Hans Naert <hans.naert@vives.be>
2024-11-21 22:03:33 +0100

    toggle relais with module onoff

--- index.js
+++ index.js
@@ -2,15 +2,10 @@ import express from 'express';
 import { createServer } from 'node:http';
 import path from 'path';
 import { Server } from 'socket.io';
+import onoff from 'onoff';
 
-import rpio  from 'rpio';
-rpio.open(15, rpio.INPUT);
-console.log('Pin 15 is currently ' + (rpio.read(15) ? 'high' : 'low'));
-
-//var relais = new pkgonoff.Gpio(17, 'out'); //use GPIO pin 17, and specify that it is output
-
-var relais=false;
-
+var relais = new onoff.Gpio(17+512, 'out'); //use GPIO pin 17, and specify that it is output
+console.log(relais);
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
@@ -30,13 +25,13 @@ io.on('connection', (socket) => {
   });
   socket.on('toggle', (msg) =>
   {
-  /*  if (relais.readSync() === 0) { //check the pin state, if the state is 0 (or off)
-      relais.writeSync(1); //set pin state to 1 (turn LED on)
+  if (relais.readSync() === 0) { //check the pin state, if the state is 0 (or off)
+      relais.writeSync(1);
+      console.log('set pin state to 1 (turn LED on)');
     } else {
-      relais.writeSync(0); //set pin state to 0 (turn LED off)
-    }*/
-   relais=! relais;
-   console.log(`toggle relais, value is ${relais}`);
+      relais.writeSync(0);
+      console.log('set pin state to 0 (turn LED off)');
+    }  
   });
   socket.on('chat message', (msg) => 
   {
